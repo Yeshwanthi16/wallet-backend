c9a92c713aed5f523268464b150cd178db204c48
Yeshwanthi16 <yashuin@gmail.com>
2023-12-22 19:04:28 +0530

    fixed cors

--- src/main/java/com/example/wallet/security/WebSecurityConfig.java
+++ src/main/java/com/example/wallet/security/WebSecurityConfig.java
@@ -1,27 +1,15 @@
 package com.example.wallet.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig   {
-
+public class WebSecurityConfig {
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-        http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-        )
-                .httpBasic(withDefaults());
-
-        return http.build();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().anyRequest();
     }
 }
